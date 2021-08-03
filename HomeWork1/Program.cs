d78aed262b3cfd803d902fefb879083d41f012f5
Alexey Vlasenko <alexinhex@gmail.com>
2021-08-03 17:38:37 +0300

    Все работает кроме взятия квадратного корня.

--- HomeWork1/Program.cs
+++ HomeWork1/Program.cs
@@ -8,6 +8,9 @@ namespace HomeWork1
         {
             string operation, NUM1, NUM2;
             double num1, num2;
+            double res = 0;
+            int numOperation;
+            
 
             Console.WriteLine("Выберите математическую операцию:");
             Console.WriteLine("1 - Сложение");
@@ -15,15 +18,90 @@ namespace HomeWork1
             Console.WriteLine("3 - Умножение");
             Console.WriteLine("4 - Деление");
             Console.WriteLine("5 - Процент который составляет одно число от другого");
-            Console.WriteLine("6 - квадратный корень");
+            Console.WriteLine("6 - квадратный корень введенного числа");
             operation = Console.ReadLine();
 
-            Console.WriteLine("Введите первое число");
-            NUM1 = Console.ReadLine();
+            bool parseResult = int.TryParse(operation, out numOperation);
 
-            Console.WriteLine("Введите второе число");
-            NUM2 = Console.ReadLine();
+            if (parseResult)
+            {
+                Console.WriteLine("Отлично!");
+            }
+            else
+            {
+                Console.WriteLine("Неверно введен номер операции.");
+            }
 
+            if (numOperation <= 5)
+            {
+                Console.WriteLine("Введите первое число");
+                NUM1 = Console.ReadLine();
+                bool parseResultNum1 = double.TryParse(NUM1, out num1);
+
+                if (parseResultNum1)
+                {
+                    Console.WriteLine("Отличный выбор!");
+                }
+                else
+                {
+                    Console.WriteLine("Введите верное первое число");
+                }
+
+                Console.WriteLine("Введите второе число");
+                NUM2 = Console.ReadLine();
+                bool parseResultNum2 = double.TryParse(NUM2, out num2);
+
+                if (parseResultNum1)
+                {
+                    Console.WriteLine("Прекрасно!");
+                }
+                else
+                {
+                    Console.WriteLine("Введите верное второе число");
+                }
+
+
+                if (numOperation == 1)
+                {
+                    res = num1 + num2;
+                }
+                else if (numOperation == 2)
+                {
+                    res = num1 - num2;
+                }
+                else if (numOperation == 3)
+                {
+                    res = num1 * num2;
+                }
+                else if (numOperation == 4)
+                {
+                    res = num1 / num2;
+                }
+                else
+                {
+                    res = num1 / num2 * 100;
+                }
+
+            }
+            else
+            {
+                Console.WriteLine("Введите число");
+                NUM1 = Console.ReadLine();
+                bool parseResultNum1 = double.TryParse(NUM1, out num1);
+
+                if (parseResultNum1)
+                {
+                    Console.WriteLine("Отличный выбор!");
+                }
+                else
+                {
+                    Console.WriteLine("Введите верное число");
+
+                    res = Math.Sqrt(num1);
+                }
+            }
+
+            Console.WriteLine("Результат операции = " + res);
         }
     }
 }
